a026f897cb2a8fa1a6a328f1d493e1da04a5a3b0
shishnk <akorund@mail.ru>
2021-12-04 01:30:43 +0700

    Добавил индексаторы

--- nMethods_4/Vector.cs
+++ nMethods_4/Vector.cs
@@ -3,29 +3,37 @@ namespace nMethods_4
     public class Vector
     {
         public real[] vec;
+        public int Length { get; init; }
 
         public Vector(int dim)
         {
             vec = new real[dim];
+            Length = dim;
+        }
+
+        public real this[int index]
+        {
+            get => vec[index];
+            set => vec[index] = value;
         }
 
         public static Vector operator *(real constant, Vector vector)
         {
-            Vector result = new(vector.vec.Length);
+            Vector result = new(vector.Length);
 
-            for (uint i = 0; i < vector.vec.Length; i++)
-                result.vec[i] = constant * vector.vec[i];
+            for (int i = 0; i < vector.Length; i++)
+                result[i] = constant * vector[i];
 
             return result;
         }
 
         public static Vector operator +(Vector fstVector, Vector sndVector)
         {
-            Vector result = new(fstVector.vec.Length);
+            Vector result = new(fstVector.Length);
 
-            for (uint i = 0; i < fstVector.vec.Length; i++)
+            for (int i = 0; i < fstVector.Length; i++)
             {
-                result.vec[i] = fstVector.vec[i] + sndVector.vec[i];
+                result[i] = fstVector[i] + sndVector[i];
             }
 
             return result;
@@ -33,12 +41,29 @@ namespace nMethods_4
 
         public static Vector operator -(Vector vector)
         {
-            Vector result = new(vector.vec.Length);
+            Vector result = new(vector.Length);
 
-            for (uint i = 0; i < vector.vec.Length; i++)
-                result.vec[i] = vector.vec[i] * (-1);
-            
+            for (int i = 0; i < vector.Length; i++)
+                result[i] = vector[i] * (-1);
+
             return result;
+        }
+
+        public static void Copy(Vector source, Vector destination)
+        {
+            for (int i = 0; i < source.Length; i++)
+                destination[i] = source[i];
+        }
+
+        public static void Clear(Vector vector)
+        {
+            for (int i = 0; i < vector.Length; i++)
+                vector[i] = 0;
+        }
+
+        public static void Resize(ref Vector vector, int lenght)
+        {
+            vector = new(lenght);
         }
     }
 }
