ed064d89726fb3f224e78d73ff32301065491dbd
shishnk <akorund@mail.ru>
2021-12-04 01:15:21 +0700

    Изменил прорисовку уровней линий

--- nMethods_4/graphics.py
+++ nMethods_4/graphics.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
 # две окружности, которые не пересекаются
-def firstTest():
+def firstTest(forLevels):
     angle = np.linspace(0, 2 * np.pi, 100)
 
     radius = 2
@@ -16,23 +16,18 @@ def firstTest():
 
     figure, axes = plt.subplots(1)
 
-    axes.plot(x1, y1, label='F1', linewidth=2, color='g')
-    axes.plot(x2, y2, label='F2', linewidth=2, color='y')
-
-    axes.legend()
-
     mesh1 = np.arange(-5, 4.1, 0.1)
     mesh2 = np.arange(-3, 3.1, 0.1)
     meshX, meshY = np.meshgrid(mesh1, mesh2)
 
     z = (meshX-2)**2 + (meshY**2) - 4 + (meshX+3)**2 + meshY**2 - 4
 
-    contourf_ = axes.contourf(meshX, meshY, z, levels=12)
+    contourf_ = axes.contourf(meshX, meshY, z, levels=forLevels)
 
     return angle, x1, y1, x2, y2, axes, figure, mesh1, mesh2, meshX, meshY, z, contourf_
 
 # две окружности, которые пересекаются в одной точке
-def secondTest():
+def secondTest(forLevels):
     angle = np.linspace(0, 2 * np.pi, 100)
 
     radius = 2
@@ -51,12 +46,12 @@ def secondTest():
 
     z = (meshX+3)**2 + (meshY**2) - 4 + (meshX-1)**2 + meshY**2 - 4
 
-    contourf_ = axes.contourf(meshX, meshY, z, levels=12)
+    contourf_ = axes.contourf(meshX, meshY, z, levels=forLevels)
 
     return angle, x1, y1, x2, y2, axes, figure, mesh1, mesh2, meshX, meshY, z, contourf_
 
 # две окружности, которые пересекаются в двух точках
-def thirdTest():
+def thirdTest(forLevels):
     angle = np.linspace(0, 2 * np.pi, 100)
 
     radius = 2
@@ -73,14 +68,14 @@ def thirdTest():
     mesh2 = np.arange(-4, 3.1, 0.1)
     meshX, meshY = np.meshgrid(mesh1, mesh2)
 
-    z = (meshX)**2 + (meshY + 2)**2 - 4 + (meshX)**2 + (meshY - 1)**2 - 4
+    z = ((meshX)**2 + (meshY + 2)**2 - 4)**2 + ((meshX)**2 + (meshY - 1)**2 - 4)**2
 
-    contourf_ = axes.contourf(meshX, meshY, z, levels=12)
+    contourf_ = axes.contourf(meshX, meshY, z, levels=forLevels)
 
     return angle, x1, y1, x2, y2, axes, figure, meshX, meshY, z, contourf_
 
 # две окружности, которые пересекаются в двух точках + прямая
-def fourthTest():
+def fourthTest(forLevels):
     angle = np.linspace(0, 2 * np.pi, 100)
 
     radius = 2
@@ -92,7 +87,7 @@ def fourthTest():
     y2 = radius * np.sin(angle) + 1
 
     lineX = np.linspace(-2.9, 1.76, 10)
-    lineY = 500.0 / 441.0 * lineX + 1
+    lineY = 500.0 / 441 * lineX + 1
 
     figure, axes = plt.subplots(1)
 
@@ -100,14 +95,14 @@ def fourthTest():
     mesh2 = np.arange(-4, 3.1, 0.1)
     meshX, meshY = np.meshgrid(mesh1, mesh2)
 
-    z = (meshX)**2 + (meshY + 2)**2 - 4 + (meshX)**2 + (meshY - 1)**2 - 4
+    z = ((meshX)**2 + (meshY + 2)**2 - 4)**2 + ((meshX)**2 + (meshY - 1)**2 - 4)**2 + (500.0/441 * meshX + 1 - meshY)**2
 
-    contourf_ = axes.contourf(meshX, meshY, z, levels=12)
+    contourf_ = axes.contourf(meshX, meshY, z, levels=forLevels)
 
     return angle, x1, y1, x2, y2, lineX, lineY, axes, figure, meshX, meshY, z, contourf_
 
 # три попарно пересекающиеся
-def fifthTest():
+def fifthTest(forLevels):
     lineX1 = np.linspace(-2.9, 1.9, 10)
     lineY1 = lineX1 + 1
 
@@ -123,14 +118,14 @@ def fifthTest():
     mesh2 = np.arange(-4, 3.1, 0.1)
     meshX, meshY = np.meshgrid(mesh1, mesh2)
 
-    z = meshX + 1 - meshY + 1.0/10 * meshX - meshY - meshX + 2 - meshY
+    z = (meshX + 1 - meshY)**2 + (1.0/10 * meshX - meshY)**2 + (-meshX + 2 - meshY)**2
 
-    contourf_ = axes.contourf(meshX, meshY, z, levels=12)
+    contourf_ = axes.contourf(meshX, meshY, z, levels=forLevels)
 
     return lineX1, lineY1, lineX2, lineY2, lineX3, lineY3, axes, figure, meshX, meshY, z, contourf_
 
 # прямая, которая пересекает синусоиду
-def sixthTest():
+def sixthTest(forLevels):
     argX = np.linspace(-10, 10, 1000)
     func = 2 + 4 * np.sin(2 * argX + 1)
 
@@ -143,9 +138,9 @@ def sixthTest():
     mesh2 = np.arange(-2.1, 5.3, 0.1)
     meshX, meshY = np.meshgrid(mesh1, mesh2)
 
-    z = 2 + 4 * np.sin(2 * meshX + 1) - meshY + meshX - meshY
+    z = (2 + 4 * np.sin(2 * meshX + 1) - meshY)**2 + (meshX - meshY)**2
 
-    contourf_ = axes.contourf(meshX, meshY, z, levels=12)
+    contourf_ = axes.contourf(meshX, meshY, z, levels=forLevels)
 
     return argX, func, lineX, lineY, figure, axes, meshX, meshY, z, contourf_
 
@@ -159,16 +154,18 @@ def main():
             x.append(float(xC))
             y.append(float(yC))
 
-    #angle, x1, y1, x2, y2, axes, figure, mesh1, mesh2, meshX, meshY, z, contourf_ = firstTest()
-    #angle, x1, y1, x2, y2, axes, figure, mesh1, mesh2, meshX, meshY, z, contourf_ = secondTest()
-    #angle, x1, y1, x2, y2, axes, figure, meshX, meshY, z, contourf_ = thirdTest()
-    #angle, x1, y1, x2, y2, lineX, lineY, axes, figure, meshX, meshY, z, contourf_ = fourthTest()
-    #lineX1, lineY1, lineX2, lineY2, lineX3, lineY3, axes, figure, meshX, meshY, z, contourf_ = fifthTest()
-    argX, func, lineX, lineY, figure, axes, meshX, meshY, z, contourf_ = sixthTest()
+    forLevels = np.linspace(0, 10, 20)
+
+    angle, x1, y1, x2, y2, axes, figure, mesh1, mesh2, meshX, meshY, z, contourf_ = firstTest(forLevels)
+    #angle, x1, y1, x2, y2, axes, figure, mesh1, mesh2, meshX, meshY, z, contourf_ = secondTest(forLevels)
+    #angle, x1, y1, x2, y2, axes, figure, meshX, meshY, z, contourf_ = thirdTest(forLevels)
+    #angle, x1, y1, x2, y2, lineX, lineY, axes, figure, meshX, meshY, z, contourf_ = fourthTest(forLevels)
+    #lineX1, lineY1, lineX2, lineY2, lineX3, lineY3, axes, figure, meshX, meshY, z, contourf_ = fifthTest(forLevels)
+    #argX, func, lineX, lineY, figure, axes, meshX, meshY, z, contourf_ = sixthTest(forLevels)
 
     # для 1-3 теста
-    #axes.plot(x1, y1, label='F1', linewidth=2, color='g')
-    #axes.plot(x2, y2, label='F2', linewidth=2, color='y')
+    axes.plot(x1, y1, label='F1', linewidth=2, color='g')
+    axes.plot(x2, y2, label='F2', linewidth=2, color='y')
     # axes.plot(lineX, lineY, linewidth=2, color='m')  # для 4-го теста
 
     # для 5-го теста
@@ -177,8 +174,8 @@ def main():
     # axes.plot(lineX3, lineY3, label='F2', linewidth=2, color='m')
 
     # для 6-го теста
-    axes.plot(argX, func, label='F1', linewidth=2, color='g')
-    axes.plot(lineX, lineY, label='F2', linewidth=4, color='m')
+    #axes.plot(argX, func, label='F1', linewidth=2, color='g')
+    #axes.plot(lineX, lineY, label='F2', linewidth=4, color='m')
 
     axes.legend()
 
@@ -196,9 +193,9 @@ def main():
     plt.plot(x, y, '-o', markersize=6, color='c')
     plt.plot(x[-1], y[-1], 'o', markersize=9, color='r')
 
-    axes.contourf(meshX, meshY, z, levels=12)
+    axes.contourf(meshX, meshY, z, levels=forLevels)
 
-    figure.colorbar(contourf_, shrink=0.93)
+    figure.colorbar(contourf_, shrink=1)
 
     plt.grid()
     axes.set_aspect(1)
