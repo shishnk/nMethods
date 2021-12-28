0a6761895d1f6931bb7da5336821137130970969
shishnk <akorund@mail.ru>
2021-12-28 21:48:48 +0700

    Внесены поправки

--- nMethods_6/Integration.cs
+++ nMethods_6/Integration.cs
@@ -1,42 +1,40 @@
 using static nMethods_6.Spline;
 
-namespace nMethods_6
-{
+namespace nMethods_6;
     public class Integration
+{
+    Vector<double> points;
+    Vector<double> weights;
+
+    public Integration()
     {
-        Vector<double> points;
-        Vector<double> weights;
+        points = new(3);
+        weights = new(3);
 
-        public Integration()
+        points[0] = 0.0;
+        points[1] = Math.Sqrt(3.0 / 5);
+        points[2] = -Math.Sqrt(3.0 / 5);
+
+        weights[0] = 8.0 / 9;
+        weights[1] = 5.0 / 9;
+        weights[2] = 5.0 / 9;
+    }
+
+    public double GaussOrder5(Basis fstPsi, Basis sndPsi, double x1, double x2)
+    {
+        double qi, pi;
+        double h = 0;
+        double result = 0;
+
+        for (int i = 0; i < 3; i++)
         {
-            points = new(3);
-            weights = new(3);
+            qi = weights[i];
+            h = Math.Abs(x2 - x1);
+            pi = (x1 + x2 + points[i] * h) / 2.0;
 
-            points[0] = 0.0;
-            points[1] = Math.Sqrt(3.0 / 5);
-            points[2] = -Math.Sqrt(3.0 / 5);
-
-            weights[0] = 8.0 / 9;
-            weights[1] = 5.0 / 9;
-            weights[2] = 5.0 / 9;
+            result += qi * fstPsi(pi, h) * sndPsi(pi, h);
         }
 
-        public double GaussOrder5(Basis fstPsi, Basis sndPsi, double x1, double x2)
-        {
-            double qi, pi;
-            double h = 0;
-            double result = 0;
-
-            for (int i = 0; i < 3; i++)
-            {
-                qi = weights[i];
-                h = Math.Abs(x2 - x1);
-                pi = (x1 + x2 + points[i] * h) / 2.0;
-
-                result += qi * fstPsi(pi, h) * sndPsi(pi, h);
-            }
-
-            return result * h / 2.0;
-        }
+        return result * h / 2.0;
     }
 }
